ca383ecb02a25337401d2c744780d37fcf525cc7
Saumay Paul <saumaypaul20@gmail.com>
2018-11-08 00:59:30 +0530

    animations improved

--- js/control.js
+++ js/control.js
@@ -1,67 +1,45 @@
+
 function myFunction() {
-        var x = document.getElementById("menu");
+    var x = document.getElementById("menu");
     if (x.style.display === "none") {
-        
-        x.classList.remove("slideOutLeft");
-        x.classList.add("slideInLeft");
+        x.classList.add("slideInLeft", true);
         x.style.display = "block";
         
     } else {
-        
-        x.classList.remove("slideInLeft");
-        x.classList.add("slideOutLeft");
         x.style.display = "none";
-        
-    }
+	}
 }
 
 function mySecondFunction() {
 	var x = document.getElementById("right_sidebar");
-if (x.style.display === "none") {
-	
-	x.classList.remove("slideOutRight");
-	x.classList.add("slideInRight");
-	x.style.display = "block";
-	// bounceIn
-} else {
-	
-	x.classList.remove("slideInRight");
-	x.classList.add("slideOutRight");
-	x.style.display = "none";
-	
-	}
+	if (x.style.display === "none") {
+		x.classList.add("slideInRight");
+		x.style.display = "block";
+	} else {
+		x.style.display = "none";
+		}
 }
 
 function myThirdFunction() {
 	var x = document.getElementById("search_responsive");
-if (x.style.display !== 'block') {
-	x.classList.add("bounceInDown");
-	x.style.display = "block";
-	
-} else {
-	
-	x.style.display = "none";
-	
+	if (x.style.display !== 'block') {
+		x.classList.add("bounceInDown");
+		x.style.display = "block";
+		x.style.padding = "15px";
+		
+	} else {	
+		x.style.display = "none";	
 	}
 }
 
 function myFourthFunction() {
 	var x = document.getElementById("notifs");
-if (x.style.display !== 'block') {
-	
-	x.style.display = "block";
-	
+if (x.style.display !== 'block') {	
+	x.style.display = "block";	
 } else {
-	x.style.display = "none";
-	
+	x.style.display = "none";	
 	}
 }
-
-
-
-
-
-	
 
 
 
